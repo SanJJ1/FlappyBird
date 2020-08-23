46aa456cad57d9ba4e1ec76970321e387d254edc
Sanjay Janardhan <21janardhansanjay@gmail.com>
2020-08-23 17:45:54 -0400

    added movement

--- second.java
+++ second.java
@@ -11,18 +11,26 @@ public class second extends JPanel implements ActionListener
      *
      */
     private static final long serialVersionUID = 1L;
+    Timer t = new Timer(5, this);
+    double x = 0, y = 0, yVel = 1, xVel = 1, grav = .1;
+
+
     private ImageIcon image;
     
     public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
         image = new ImageIcon("imgs/yellowbird-midflap.png");
-        image.paintIcon(this, g, 0, 0);
+        image.paintIcon(this, g, (int)x, (int)y);
+        t.start();
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-
+    public void actionPerformed(ActionEvent e)
+    {
+        x += xVel;
+        y += yVel;
+        yVel += grav;
+        repaint();
     }
 }
